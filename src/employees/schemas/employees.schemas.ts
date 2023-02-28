71b0b5c2eb21ca3f43a896255a962c33f776219e
Nathachai Rungsaeng <77932024+nathachai13011997@users.noreply.github.com>
2023-02-28 13:54:34 +0700

    จัด code

--- src/employees/schemas/employees.schemas.ts
+++ src/employees/schemas/employees.schemas.ts
@@ -1,39 +1,41 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { HydratedDocument } from 'mongoose'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
 
-export type EmployeesDocument = HydratedDocument<Employees>
+export type EmployeesDocument = HydratedDocument<Employees>;
 
 @Schema()
 export class General {
-    @Prop({ required: true})
-    weight: number
+  @Prop({ required: true })
+  weight: number;
 
-    @Prop({ required: true })
-    height: number
+  @Prop({ required: true })
+  height: number;
 
-    @Prop({ required: true })
-    gender: string
+  @Prop({ required: true })
+  gender: string;
 }
 
-@Schema()
+@Schema({
+  timestamps: true,
+})
 export class Employees {
-    @Prop({ required: true})
-    name: string
+  @Prop({ required: true })
+  name: string;
 
-    @Prop({ required: true })
-    salary: number
+  @Prop({ required: true })
+  salary: number;
 
-    @Prop({ required: true })
-    address: string
+  @Prop({ required: true })
+  address: string;
 
-    @Prop({ required: true })
-    general: General
+  @Prop({ required: true })
+  general: General;
 
-    @Prop([String])
-    social: string[]
+  @Prop([String])
+  social: string[];
 
-    @Prop({ required: true })
-    department: string
+  @Prop({ required: true })
+  department: string;
 }
 
-export const EmployeesSchema = SchemaFactory.createForClass(Employees)
+export const EmployeesSchema = SchemaFactory.createForClass(Employees);
